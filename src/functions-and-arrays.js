f94933aac793fb9b64c0d00ad2db824d66d9170b
nathidaum <nathidaum@gmail.com>
2024-10-03 15:19:18 +0200

    some minor tweaks

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -17,7 +17,7 @@ function findLongestWord(arr) {
         return null
     }
     
-    let longestWord = ' ';
+    let longestWord = '';
     for (let i = 0; i < arr.length; i++) {
         if(arr[i].length > longestWord.length) {
             longestWord = arr[i];
